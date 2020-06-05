4204c5678b7190c42cec4384de38aacb7130ccec
Matthew Spofford <matthewspofford@verizon.net>
2020-06-05 13:16:40 -0400

    Begun initial work on f-test studiable addon using outlined SDK from Mountains

---
+++ ftest-addon.py
@@ -0,0 +1,21 @@
+from imountains import constants
+
+type = constants.kAddonReserve
+publicname = {constants.kDefaultLanguage:'F-Test',
+              constants.kLangEnglish:'F-Test'}
+input_type = constants.IDFF_DS_SURF_SURFACE
+
+class version_infos :
+  VERSION = '0.1.0'
+  CONTACT = 'https://github.com/MatthewSpofford'
+
+def CreateAddon() :
+  
+
+def GetCLSID(self) :
+  
+
+def OnInit(self, studiable: imountains.IStudiable,
+           paramhandler: imountains.IAddonParameterHandler, 
+           reservectl) -> bool :
+  
